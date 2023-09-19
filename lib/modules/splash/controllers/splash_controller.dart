972b960f0423cb1a3432d8bbdcc688f0efd6a7e7
VN The Boss Baby <93357243+QuDaMyker@users.noreply.github.com>
2023-09-19 13:24:27 +0700

    init splah

--- lib/modules/splash/controllers/splash_controller.dart
+++ lib/modules/splash/controllers/splash_controller.dart
@@ -1,1 +1,19 @@
 //splash controller
+
+import 'package:flutter/material.dart';
+import 'package:smart_rent/modules/home/views/home_screen.dart';
+
+Future<void> navigatorHomeScreen(BuildContext context) async {
+  Future.delayed(
+    const Duration(
+      seconds: 3,
+    ),
+    () {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctx) => const HomeScreen(),
+        ),
+      );
+    },
+  );
+}
---
+++ lib/modules/splash/views/splash_screen.dart
@@ -0,0 +1,103 @@
+//splash page
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:smart_rent/modules/home/views/home_screen.dart';
+import 'package:smart_rent/modules/splash/controllers/splash_controller.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({
+    super.key,
+  });
+
+  @override
+  State<SplashScreen> createState() {
+    return _SplashScreenState();
+  }
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    // Future.delayed(
+    //   const Duration(
+    //     seconds: 3,
+    //   ),
+    //   () {
+    //     Navigator.of(context).push(
+    //       MaterialPageRoute(
+    //         builder: (ctx) => const HomeScreen(),
+    //       ),
+    //     );
+    //   },
+    // );
+    navigatorHomeScreen(context);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(197, 138, 198, 230),
+                Color.fromARGB(251, 10, 130, 185)
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset('assets/images/ic_splash.svg'),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Smart Rent House',
+                style: GoogleFonts.ubuntuCondensed(
+                  textStyle: const TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 120.0),
+                child: LinearProgressIndicator(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                    Color.fromARGB(197, 138, 198, 230),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
